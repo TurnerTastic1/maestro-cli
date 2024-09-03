db9ee3f1708bf8c12c065e7be05baca253cf35d8
Turner Naef <Turner.Naef@gmail.com>
2024-09-03 14:45:37 -0600

    Source a configuration from a file

--- servicegenie/main.py
+++ servicegenie/main.py
@@ -17,13 +17,13 @@ import os
 import subprocess
 from datetime import datetime
 
-DATA_FILE = 'containers.json'
+DATA_FILE = 'ServiceGenieConfig.json'
 
 class ServiceGenie(cmd.Cmd):
     intro = 'Welcome to the ServiceGenie CLI. Type help or ? to list commands.\n'
     prompt = '\033[95m(ServiceGenie)\033[0m '
 
-    def __init__(self):
+    def __init__(self, data_file=DATA_FILE):
         super().__init__()
         self.data = self.load_data()
 
@@ -31,19 +31,18 @@ class ServiceGenie(cmd.Cmd):
         if os.path.exists(DATA_FILE):
             with open(DATA_FILE, 'r') as file:
                 return json.load(file)
-        return {}
+        else:
+            print("No data file found. Starting with an empty data store.")
+            return {}
     
     def save_data(self):
         with open(DATA_FILE, 'w') as file:
             json.dump(self.data, file, indent=4)
 
-    def add_container(self, container_name, workspace):
-        if container_name in self.data:
-            print(f"Error: Container \"{container_name}\" already exists in store.")
-            return
-
+    def add_container(self, container_name, container_working_dir,  workspace):
         self.data[container_name] = {
             'workspace': workspace,
+            'container_working_dir': container_working_dir,
             'last_updated': datetime.now().isoformat()  # Add the current timestamp
         }
         self.save_data()
@@ -51,11 +50,22 @@ class ServiceGenie(cmd.Cmd):
 
     def do_add(self, arg):
         'Add a Docker container name and it\'s codes workspace on the local machine: add'
-        # args = arg.split(maxsplit=1)
 
+        # Prompt the user for the container name
         container_name = input("Enter the Docker container name: ").strip()
         if not container_name:
             print("Error: Container name cannot be empty.")
+            return
+        
+        # Check if the container already exists in the data store
+        if container_name in self.data:
+            print(f"Error: Container \"{container_name}\" already exists in store.")
+            return
+        
+        # Prompt the user for the container working directory
+        container_working_dir = input("Enter the container's working directory: ").strip()
+        if not container_working_dir:
+            print("Error: Container working directory cannot be empty.")
             return
 
         # Prompt the user for the workspace
@@ -64,7 +74,28 @@ class ServiceGenie(cmd.Cmd):
             print("Error: workspace cannot be empty.")
             return
         
-        self.add_container(container_name, workspace)
+        self.add_container(container_name, container_working_dir, workspace)
+    
+    def do_configure(self, arg):
+        'Configure ServiceGenie with a JSON file: configure'
+        file_path = os.path.expanduser(input("Enter the path to the configuration file: ").strip())
+        if not file_path:
+            print("Error: File path cannot be empty.")
+            return
+        else:
+            try:
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+                    self.data.update(data)
+                    self.save_data()
+                    print("ServiceGenie configured successfully.")
+            except FileNotFoundError:
+                print("Error: File not found.")
+            except json.JSONDecodeError:
+                print("Error: Invalid JSON file.")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+        
 
     def container_exists(self, container_name):
         result = subprocess.run(['docker', 'ps', '-a', '--format', '{{.Names}}'], capture_output=True, text=True)
@@ -115,7 +146,6 @@ class ServiceGenie(cmd.Cmd):
         'List all stored container names and the workspaces of their code on the local machine: ls'
         self.do_list(arg)
         
-
     def do_rm(self, arg):
         'Remove a stored Docker container\'s data: rm CONTAINER_NAME || rm -a'
 
@@ -158,7 +188,7 @@ class ServiceGenie(cmd.Cmd):
         'Exit the CLI'
         return True
     
-    def copy_files(self, container_name, workspace):
+    def copy_files(self, container_name, container_working_dir, workspace):
         try:
             # Expand tilde to the full home directory path if present
             workspace = os.path.expanduser(workspace)
@@ -170,7 +200,7 @@ class ServiceGenie(cmd.Cmd):
                 print(f"Error: Container \"{container_name}\" does not exist.")
                 return
             
-            subprocess.run(['docker', 'cp', f'{workspace}/.', f'{container_name}:/app'], check=True)
+            subprocess.run(['docker', 'cp', f'{workspace}/.', f'{container_name}:/{container_working_dir}'], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error occurred while copying files: {e}")
         except Exception as e:
@@ -192,9 +222,11 @@ class ServiceGenie(cmd.Cmd):
         if not arg:
             print("Copying all container's code...")
             for container_name, info in self.data.items():
+                # TODO: throw errors if workspace or container_working_dir is not present
                 workspace = info.get('workspace', '-')
+                container_working_dir = info.get('container_working_dir', '-')
                 print(f"Copying code to container: {container_name} from code workspace: {workspace}")
-                self.copy_files(container_name, workspace)
+                self.copy_files(container_name, container_working_dir, workspace)
                 self.update_timestamp(container_name)
         else:
             containers = arg.split(',')
